f6c721c1ee8fd315d36cb523541dd081f4c25a6a
Enrico Thies <enrico.thies@gmail.com>
2015-05-07 23:48:32 +0200

    init bundle

---
+++ ToaAsseticBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Toa\Bundle\AsseticBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ToaAsseticBundle
+ *
+ * @author Enrico Thies <enrico.thies@gmail.com>
+ */
+class ToaAsseticBundle extends Bundle
+{
+}
